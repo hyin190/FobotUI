ea86646e9c6c38dd4539338b08dce30128be87fb
qcao819 <qcao819@aucklanduni.ac.nz>
2021-08-16 18:29:04 +1200

    renew app.js

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,6 +1,7 @@
 import Login from './components/login';
 import Register from "./components/register";
 // import './App.css';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,37 +9,45 @@ import {
   Redirect
 } from "react-router-dom";
 
+import Addproduct from './components/sections/Addproductcontent';
+import Addproductcontent from './components/sections/Addproductcontent';
+
 function App() {
   return (
     // <div className="App">
     //   <header className="App-header">
-    //     {/* <Login></Login> */}
-    //     <Register></Register>
+    //     <Addproductcontent></Addproductcontent>
         
     //   </header>
     // </div>
-    <Router>
+    // <Router>
+    //   <Switch>
+    //     <div>
+    //     <Route exact path='/'>
+    //         <div>
+    //             <Register></Register>
+    //         </div>
+    //       </Route>
+
+    //       <Route exact path='/register'>
+    //         <div className="App">
+    //             <Register></Register>
+    //         </div>
+    //       </Route>
+
+    //       <Route exact path='/login'>
+    //         <div className="App">
+    //             <Login/>
+    //         </div>
+    //       </Route>
+
+    //     </div>
+    //   </Switch>
+    // </Router>
+    <Router basename={'/chatbot'}>
       <Switch>
-        <div>
-        <Route exact path='/'>
-            <div>
-                <Register></Register>
-            </div>
-          </Route>
-
-          <Route exact path='/register'>
-            <div className="App">
-                <Register></Register>
-            </div>
-          </Route>
-
-          <Route exact path='/login'>
-            <div className="App">
-                <Login/>
-            </div>
-          </Route>
-
-        </div>
+        <Route exact path="/" component={Addproductcontent} />
+        <Route path="/add-product" component={Addproductcontent} />
       </Switch>
     </Router>
   );
