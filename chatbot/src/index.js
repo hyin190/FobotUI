598739cad282cc2f5cff0a126c422a26278ece95
qcao819 <qcao819@aucklanduni.ac.nz>
2021-08-16 18:31:47 +1200

    index.js
    new import

--- chatbot/src/index.js
+++ chatbot/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+
+
+import './assets/css/style.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={'/chatbot'}>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
